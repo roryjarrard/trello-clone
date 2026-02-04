4f0959f459e0076756e5132460c742719db1e6ce
Rory Jarrard <roryjarrard@gmail.com>
2026-02-04 13:48:37 -0700

    remove boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <h1>Welcome to the Trello Clone</h1>
-      </div>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+  return null;
+};
